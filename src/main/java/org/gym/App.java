3b3b23f11353a41e148eefdda7851ad39b2d8629
mohankrishnamachireddy <machi1m@cmich.edu>
2024-06-03 11:45:15 -0400

    Tweaked for Back

--- src/main/java/org/gym/App.java
+++ src/main/java/org/gym/App.java
@@ -11,18 +11,25 @@ public class App
     public static void main( String[] args )
     {
         Combination combination_1 = new Combination(
-                getLinkedHashSet(MuscleGroup.CHEST,MuscleGroup.BACK)
+                getLinkedHashSet(MuscleGroup.TRICEPS,MuscleGroup.BACK)
         );
         Combination combination_2 = new Combination(
-                getLinkedHashSet(MuscleGroup.TRICEPS,MuscleGroup.BICEPS,MuscleGroup.SHOULDERS)
+                getLinkedHashSet(MuscleGroup.BICEPS,MuscleGroup.CHEST)
         );
         Combination combination_3 = new Combination(
-                getLinkedHashSet(MuscleGroup.LEGS,MuscleGroup.CALVES)
+                getLinkedHashSet(MuscleGroup.TRICEPS,MuscleGroup.BACK)
         );
-        List<Combination> combinations = new ArrayList<>(Arrays.asList(combination_1, combination_2, combination_3));
+        Combination combination_4 = new Combination(
+                getLinkedHashSet(MuscleGroup.BICEPS,MuscleGroup.SHOULDERS)
+        );
+        Combination combination_5 = new Combination(
+                getLinkedHashSet(MuscleGroup.TRICEPS,MuscleGroup.LEGS)
+        );
+        List<Combination> combinations = new ArrayList<>(Arrays.asList(combination_1, combination_2, combination_3
+        , combination_4, combination_5));
 
         int input = Integer.parseInt(args[0]);
-        int rotation = input % 3;
+        int rotation = input % 5;
 
         Combination todaysCombination = combinations.get(rotation);
 
